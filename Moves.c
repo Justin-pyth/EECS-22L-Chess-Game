55603043995a77a17060cf5e708595fdaf72c518
Kat Go <katgo@Kats-MacBook-Pro.local>
2026-04-14 14:50:12 -0700

    Added functions for each piece type's move

--- Moves.c
+++ Moves.c
@@ -1,16 +1,83 @@
 #include "Moves.h"
 
-void getMoves(int* moveCount)
+
+struct move* getMoves(int* moveCount)
 {
-    ;
+   ;
 }
 
+
+
+
+/*
+   Returns a list of legal moves that the specified piece can perform
+*/
 struct move* getLegalMoves(struct piece p, int* moveCount)
 {
-    ;
+   ;
 }
 
-bool isLegalMove(struct move moveMaid, const struct gameState* gs)
+
+/*
+   Checks if the specified move for that piece is legal
+*/
+bool isLegalMove(struct move moveMade, const struct gameState* gs)
 {
-    ;
+   ;
 }
+
+
+// Pawn
+struct move* getPawnMoves(struct piece p, int* moveCount)
+{
+  
+}
+
+
+// Bishop
+struct move* getBishopMoves(struct piece p, int* moveCount)
+{
+
+
+}
+
+
+// Knight
+struct move* getKnightMoves(struct piece p, int* moveCount)
+{
+
+
+}
+
+
+// Rook
+struct move* getRookMoves(struct piece p, int* moveCount)
+{
+
+
+}
+
+
+// Queen
+struct move* getQueenMoves(struct piece p, int* moveCount)
+{
+
+
+}
+
+
+// King
+struct move* getKingMoves(struct piece p, int* moveCount)
+{
+
+
+}
+
+
+// Anteater
+struct move* getAnteaterMoves(struct piece p, int* moveCount)
+{
+
+
+}
+
