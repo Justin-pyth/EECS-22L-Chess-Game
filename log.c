75514f7472ff2222be96c8c0c7611fce1b98b624
Justin-pyth <justinxsds357@gmail.com>
2026-04-18 21:21:15 -0700

    Adding header file functions

--- log.c
+++ log.c
@@ -2,3 +2,9 @@
 #include "Moves.h"
 #include "Engine.h"
 
+void logMove (struct pos position, struct piece* Piece, struct log logOfMoves);
+
+
+char[] convertLogMove (struct log Log);
+
+void convertPos (struct pos position);
--- log.h
+++ log.h
@@ -1,13 +1,20 @@
 #ifndef LOG_H
 #define LOG_H
 
-void logMove (struct pos position, struct piece* Piece, struct log logOfMoves);
+struct log {
+    string move; // "a1 - j10"
+    int piece; // Black or White
+    static int moveNumber; // 1-1000 for # of moves in the game
+    int history[1000];
+};
 
+//Logs move made by chess user into the log
+void logMove (struct pos position, struct piece* Piece, const struct log logOfMoves);
 
-char[] convertLogMove (struct log Log);
+//Converts move in the log to chess notation for the user
+char[] convertLogMove (const struct log* Log);
 
+//Converts the position characters into ints to be used on the board
 void convertPos (struct pos position);
-
-
 
 #endif
