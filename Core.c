effe980675c83c8492ab59b1ee3c3ac1b0e6df84
Rishab-S-K <rishabsk2@gmail.com>
2026-04-28 10:20:27 -0700

    Can't castle if king is in check.

--- Core.c
+++ Core.c
@@ -273,6 +273,8 @@ bool isCastlingValid(struct piece* board[8][10], enum pieceColor color,
         if (board[row][c]) return false;
 
     enum pieceColor opp  = (color == WHITE) ? BLACK : WHITE;
+    if (isKingInCheck(board, color)) return false;
+
     int destCol = kingSide ? 7 : 3;
     int step = (destCol > kingCol) ? 1 : -1;
     struct piece* kPiece = board[row][kingCol];
